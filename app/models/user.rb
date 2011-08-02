0c8ad32ea1a199ad080dda9ca895f3e1283f45ca
James Stewart <jys@ketlai.co.uk>
2011-08-02 16:40:04 +0100

    Add a user model. Our admin will use a very similar set up to the publisher app

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+class User
+  include Mongoid::Document
+  include GDS::SSO::User
+
+  cache
+
+  field  :uid, :type => String
+  field  :email, :type => String
+  field  :version, :type => Integer
+  field  :name, :type => String
+
+  def record_action(data_set, type, comment=nil)
+    action = data_set.new_action(self, type, comment)
+    # NoisyWorkflow.make_noise(edition.container,action).deliver
+  end
+
+  def self.find_by_uid(uid)
+    first(conditions: {uid: uid})
+  end
+  
+  def activate_data_set(data_set)
+    data_set.activate!
+    record_action(data_set, 'activated')
+  end
+end
