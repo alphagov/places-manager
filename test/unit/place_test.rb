bdde5579fb79b66168f846a33d492f43838b5e1a
James Stewart <jys@ketlai.co.uk>
2011-08-08 15:50:59 +0100

    Add some basic tests for Place

--- test/unit/place_test.rb
+++ test/unit/place_test.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
 
 class PlaceTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "responds to full_address with a compiled address" do
+    p = Place.new(:name => 'Hercules House', :address => '1 Hercules Road', :town => 'London', :postcode => 'SE1 7DU')
+    assert_equal '1 Hercules Road, London, SE1 7DU, UK', p.full_address
+  end
+  
+  test "can find distance from a given longitude/latitude" do
+    p = Place.new(:location => [0,0])
+    assert_equal 3454.9999999999995, p.distance_from(50, 0)
   end
 end
