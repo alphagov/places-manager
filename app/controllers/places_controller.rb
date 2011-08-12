2393c90680e197ddddd49260adf56ce4df04bb5d
James Stewart <jys@ketlai.co.uk>
2011-08-12 10:44:01 +0100

    Allow signed in users to export any version of the data using the API

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -12,8 +12,21 @@ class PlacesController < ApplicationController
     else
       args = { :limit => 50 }
     end
+    
+    if user_signed_in? and params[:version].present?
+      data_set = @service.data_sets.find(id: params[:version])
+    else
+      data_set = @service.active_data_set
+    end
+    
+    head 404 and return if data_set.nil?
 
-    @places = @service.active_data_set.places_near(params[:lat].to_f, params[:lng].to_f, args)
+    if params[:lat].present?
+      @places = data_set.places_near(params[:lat].to_f, params[:lng].to_f, args)
+    else
+      @places = data_set.places.all
+    end
+
     respond_with(@places)
   end
 end
