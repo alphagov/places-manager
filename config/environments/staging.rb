555a78154c521e8ee72b2eea362b960c53004183
James Stewart <jys@ketlai.co.uk>
2011-08-15 10:08:10 +0100

    Use proper naming for our mapit/gazetteer setup

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -5,10 +5,10 @@ Imminence::Application.configure do
   config.action_mailer.smtp_settings = {:enable_starttls_auto => false}
   
   Geogov.configure do |g|
-    g.provider_for :centre_of_country,             DracosGazetteer.new("http://gazetteer.alpha.gov.uk")
-    g.provider_for :centre_of_district,            Mapit.new("http://mapit.alpha.gov.uk")
-    g.provider_for :areas_for_stack_from_postcode, Mapit.new("http://mapit.alpha.gov.uk")
-    g.provider_for :areas_for_stack_from_coords,   Mapit.new("http://mapit.alpha.gov.uk")
-    g.provider_for :lat_lon_from_postcode,         Mapit.new("http://mapit.alpha.gov.uk")
+    g.provider_for :centre_of_country,             Geogov::DracosGazetteer.new("http://gazetteer.alpha.gov.uk")
+    g.provider_for :centre_of_district,            Geogov::Mapit.new("http://mapit.alpha.gov.uk")
+    g.provider_for :areas_for_stack_from_postcode, Geogov::Mapit.new("http://mapit.alpha.gov.uk")
+    g.provider_for :areas_for_stack_from_coords,   Geogov::Mapit.new("http://mapit.alpha.gov.uk")
+    g.provider_for :lat_lon_from_postcode,         Geogov::Mapit.new("http://mapit.alpha.gov.uk")
   end
 end
