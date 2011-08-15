f81a5ca8c46c78858e0a1e1ab36b932393a9fe8c
James Stewart <jys@ketlai.co.uk>
2011-08-15 11:51:04 +0100

    More changes to providers for various geo services

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -5,7 +5,7 @@ Imminence::Application.configure do
   config.action_mailer.smtp_settings = {:enable_starttls_auto => false}
   
   Geogov.configure do |g|
-    g.provider_for :centre_of_country,             Geogov::DracosGazetteer.new("http://gazetteer.alpha.gov.uk")
+    g.provider_for :centre_of_country,             Geogov::Geonames.new
     g.provider_for :centre_of_district,            Geogov::Mapit.new("http://mapit.alpha.gov.uk")
     g.provider_for :areas_for_stack_from_postcode, Geogov::Mapit.new("http://mapit.alpha.gov.uk")
     g.provider_for :areas_for_stack_from_coords,   Geogov::Mapit.new("http://mapit.alpha.gov.uk")
