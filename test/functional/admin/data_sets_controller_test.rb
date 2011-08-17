e7984cd11ee81d8a6b835bcf6a679605a1f094cc
James Stewart <jys@ketlai.co.uk>
2011-08-17 15:38:55 +0100

    Another change to make tests clearer

--- test/functional/admin/data_sets_controller_test.rb
+++ test/functional/admin/data_sets_controller_test.rb
@@ -1,9 +1,6 @@
 require 'test_helper'
 
 class Admin::DataSetsControllerTest < ActionController::TestCase
-  def setup
-    @request.env['HTTP_REFERER'] = "http://localhost:3000/admin/services/#{setup_service.id}"
-  end
 
   def setup_service
     Service.first || Service.create(:name => 'Important Government Service', :slug => 'important-government-service')
@@ -11,6 +8,7 @@ class Admin::DataSetsControllerTest < ActionController::TestCase
     
   test "it handles invalid CSV files gracefully" do
     as_logged_in_user do
+      @request.env['HTTP_REFERER'] = "http://localhost:3000/admin/services/#{setup_service.id}"
       csv_file = fixture_file_upload(Rails.root.join('test/fixtures/bad_csv.csv'), 'text/csv')
       post :create, :service_id => setup_service.id, :data_set => {:data_file => csv_file}
       assert_response :redirect
