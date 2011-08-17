3ed4a1a6a917fa0d1323f5f0c6ac971c2248ce99
James Stewart <jys@ketlai.co.uk>
2011-08-17 15:36:35 +0100

    Slightly cleaner/clearer tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'mocha'
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
   #
@@ -10,4 +11,10 @@ class ActiveSupport::TestCase
   # fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
+  def as_logged_in_user(&block)
+    @controller.stubs(:authenticate_user!).returns(true)
+    yield
+    @controller.unstub(:authenticate_user!)
+  end
 end
