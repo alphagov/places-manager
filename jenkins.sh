bf0e84a62c0669569605ff3623bc1d3c2ffa4323
Gareth Rushgrove <gareth@alphagov.co.uk>
2011-09-09 11:27:57 +0000

    add rails stats output for jenkins

--- jenkins.sh
+++ jenkins.sh
@@ -3,6 +3,7 @@ source '/usr/local/lib/rvm'
 bundle install --path "/home/jenkins/bundles/${JOB_NAME}" --deployment
 bundle exec rake db:setup
 bundle exec rake db:migrate
+bundle exec rake stats
 export DISPLAY=:99
 /etc/init.d/xvfb start
 bundle exec rake ci:setup:testunit test:units test:functionals test:integration 
