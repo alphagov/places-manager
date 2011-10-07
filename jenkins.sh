2595fc533c279a9f4f5fa8ec201649c1535f01b1
Paul Battley <pbattley@gmail.com>
2011-10-07 10:20:12 +0100

    Leave xvfb running all the time.
    Starting and stopping xvfb will break if two projects run their tests
    simultaneously, and is in any case unnecessary.

--- jenkins.sh
+++ jenkins.sh
@@ -5,8 +5,6 @@ bundle exec rake db:setup
 bundle exec rake db:migrate
 bundle exec rake stats
 export DISPLAY=:99
-/etc/init.d/xvfb start
 bundle exec rake ci:setup:testunit test:units test:functionals test:integration 
 RESULT=$?
-/etc/init.d/xvfb stop
 exit $RESULT
