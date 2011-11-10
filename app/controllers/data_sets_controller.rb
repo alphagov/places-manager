9ac075ec24aec19455887e6237fdf573ad940db6
Craig R Webster <craig@barkingiguana.com>
2011-11-10 14:18:25 +0000

    Provide a JSON hash with an id attribute

--- app/controllers/data_sets_controller.rb
+++ app/controllers/data_sets_controller.rb
@@ -1,36 +1,36 @@
 class DataSetsController < ApplicationController
   def show
     respond_to do |format|
-      format.xml { render :xml => data_set }
+      # TODO: Provide other formats eg with to_xml
       format.json { render :json => data_set }
     end
   end
 
   PUBLIC_BODIES = [
-    "Attorney general's office",
-    "Cabinet office",
-    "Department for business, innovation and skills",
-    "Department for communities and local government",
-    "Department for culture, media and sport",
-    "Department for education",
-    "Department for environment, food and rural affairs",
-    "Department for international development",
-    "Department for transport",
-    "Department for work and pensions",
-    "Department of energy and climate change",
-    "Department of health",
-    "Foreign and commonwealth office",
-    "HM treasury",
-    "HM revenue & customs",
-    "Home office",
-    "Ministry of defence",
-    "Ministry of justice",
-    "Northern Ireland office",
-    "Office of the advocate general for Scotland",
-    "Office of the leader of the house of commons",
-    "Privy council office",
-    "Scotland office",
-    "Wales office",
+    { :id => "Attorney general's office" },
+    { :id => "Cabinet office" },
+    { :id => "Department for business, innovation and skills" },
+    { :id => "Department for communities and local government" },
+    { :id => "Department for culture, media and sport" },
+    { :id => "Department for education" },
+    { :id => "Department for environment, food and rural affairs" },
+    { :id => "Department for international development" },
+    { :id => "Department for transport" },
+    { :id => "Department for work and pensions" },
+    { :id => "Department of energy and climate change" },
+    { :id => "Department of health" },
+    { :id => "Foreign and commonwealth office" },
+    { :id => "HM treasury" },
+    { :id => "HM revenue & customs" },
+    { :id => "Home office" },
+    { :id => "Ministry of defence" },
+    { :id => "Ministry of justice" },
+    { :id => "Northern Ireland office" },
+    { :id => "Office of the advocate general for Scotland" },
+    { :id => "Office of the leader of the house of commons" },
+    { :id => "Privy council office" },
+    { :id => "Scotland office" },
+    { :id => "Wales office" },
   ].freeze
 
   WRITING_TEAMS = [
