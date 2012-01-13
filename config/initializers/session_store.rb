0fcde5be0f6dffbcf2df6b7fd0307fa1bff0dffd
James Stewart <jys@ketlai.co.uk>
2012-01-13 12:49:32 +0000

    Set cookies to only be served over https and to be http only

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Imminence::Application.config.session_store :cookie_store, :key => '_imminence_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Imminence::Application.config.session_store :active_record_store
+Imminence::Application.config.session_store :cookie_store,
+  :key => '_imminence_session'
+  :secure => Rails.env.production?,
+  :http_only => true
