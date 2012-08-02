ec5ee64d0312ccfe77ba1bb69c7a77b08cdd60a4
James Stewart <jys@ketlai.co.uk>
2012-08-02 10:03:24 +0100

    Simplify jenkins.sh and make sure it runs all our tests

--- jenkins.sh
+++ jenkins.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -x
+export RAILS_ENV=test
 bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
-bundle exec rake db:setup
-bundle exec rake db:migrate
-bundle exec rake stats
-export DISPLAY=:99
-bundle exec rake ci:setup:testunit test:units test:functionals test:integration 
-RESULT=$?
-exit $RESULT
+bundle exec rake db:drop db:create db:schema:load
+bundle exec rake
