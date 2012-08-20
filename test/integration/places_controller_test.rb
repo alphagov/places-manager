d78948595ecb4455ceb315456ae1feb165e8d574
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2012-08-20 14:30:36 +0100

    Pull the places controller test into integration.
    Since it's testing the functionality across all layers (to some degree
    or other), it really belongs in with the integration tests.

--- test/integration/places_controller_test.rb
+++ test/integration/places_controller_test.rb
@@ -1,13 +1,17 @@
 require 'test_helper'
 
 class PlacesControllerTest < ActionController::TestCase
-  def setup_service
-    Service.first || Service.create(:name => 'Important Government Service', :slug => 'important-government-service')
+
+  setup do
+    @service = Service.create(
+      name: "Important Government Service",
+      slug: "important-government-service"
+    )
   end
-  
+
   test "as a logged in user I can access a non-active data set" do
     as_logged_in_user do
-      get :show, :id => setup_service.slug, :format => :json
+      get :show, id: @service.slug, format: :json
       assert_response :success
     end
   end
