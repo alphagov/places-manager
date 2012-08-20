e43ed19a5500bd92ae63099a9c8f2660703bad3e
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2012-08-20 16:34:27 +0100

    Disable Slimmer for tests.
    Since we're going to be removing Slimmer from this app in the (hopefully
    near) future, the temporary hackery of disabling it for testing is one
    we can live with.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+
   include GDS::SSO::ControllerMethods
+
+  if Rails.env.test?
+    before_filter do
+      headers["X-Slimmer-Skip"] = "absolutely"
+    end
+  end
 end
