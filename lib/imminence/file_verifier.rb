fbc8838ab0f90232c89ec69c30f59eb6a26848ce
James Stewart <jys@ketlai.co.uk>
2012-10-01 21:17:48 +0100

    Extend FileVerifier class to give access to type and sub_type
    This allows us to be more permissive in the files we'll accept.

--- lib/imminence/file_verifier.rb
+++ lib/imminence/file_verifier.rb
@@ -10,14 +10,22 @@ module Imminence
       end
     end
 
-    def get_mime_type
+    def type
+      mime_type.split('/').first
+    end
+
+    def sub_type
+      mime_type.split('/').last
+    end
+
+    def mime_type
       shell_result = IO.popen(["file", "--brief", "--mime-type", filename],
         in: :close, err: :close)
       shell_result.read.chomp
     end
 
-    def is_mime_type?(mime_type)
-      get_mime_type == mime_type
+    def is_mime_type?(comparison_mime_type)
+      mime_type == comparison_mime_type
     end
   end
 end
