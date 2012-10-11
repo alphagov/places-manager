6f47b0a03fcd23bd6a60da337fbb348be3f9e524
John Griffin <johnog@gmail.com>
2012-10-11 10:52:29 +0100

    Prefix stats events with api or admin

--- config/initializers/statsd.rb
+++ config/initializers/statsd.rb
@@ -5,8 +5,16 @@ class Imminence::StatsCollector
     end
   end
 
+  def self.prefix_controller(name)
+    if name.start_with?("admin/")
+      return name.gsub("admin/", "admin.")
+    end
+    "api.#{name}"
+  end
+
   def self.timing(time, controller, action)
-    statsd.timing("response_time.#{controller}.#{action}", time)
+    prefixed_controller = prefix_controller(controller)
+    statsd.timing("response_time.#{prefixed_controller}.#{action}", time)
   end
 end
 
