49a8a6c5a05c4e3b3a1cf1416722fcf678ceb8e4
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2012-10-26 16:20:19 +0000

    Add relationless criteria
    Retrieve schemes where relational collections are empty or have a one of the relational params.

--- app/controllers/business_support_schemes_controller.rb
+++ app/controllers/business_support_schemes_controller.rb
@@ -1,11 +1,14 @@
 class BusinessSupportSchemesController < ApplicationController
 
+  RELATIONAL_KEYS = [:business_types, :sectors, :stages, :locations, :types]
+
   def index
-    args = any_in_args
-    if args.empty?
+    criteria = schemes_criteria
+    if criteria.empty?
       schemes = BusinessSupportScheme.asc(:title)
     else
-      schemes = BusinessSupportScheme.any_in(args).asc(:title)
+      relations = params.keep_if{ |k,v| RELATIONAL_KEYS.include?(k.to_sym) }
+      schemes = BusinessSupportScheme.where({ "$and" => schemes_criteria }).asc(:title)
     end 
     @count = schemes.size
     @schemes_json = schemes.to_json(only: [:business_support_identifier, :title]) 
@@ -16,16 +19,22 @@ class BusinessSupportSchemesController < ApplicationController
 
   private
 
-  def any_in_args
-    args = {}
-    [:business_types, :sectors, :stages, :locations, :types].each do |sym|
+  def schemes_criteria
+    criteria = []
+    RELATIONAL_KEYS.each do |sym|
       unless params[sym].nil?
-        args["business_support_#{sym.to_s.singularize}_ids".to_sym] = ids_for_slugs(sym)
+        collection = "business_support_#{sym.to_s.singularize}_ids".to_sym
+        collection_ids = ids_for_slugs(sym)
+        selector = { collection => [] }
+        unless collection_ids.empty?
+          selector = { "$or" => [{ collection => { "$in" => collection_ids } }, selector] }
+        end
+        criteria << selector
       end
     end
-    args
+    criteria
   end
-  
+
   def ids_for_slugs(sym)
     klass = Kernel.const_get("BusinessSupport#{sym.to_s.singularize.camelize}")
     klass.any_in(slug: params[sym].split(',')).map(&:id)  
--- test/functional/business_support_schemes_controller_test.rb
+++ test/functional/business_support_schemes_controller_test.rb
@@ -17,6 +17,8 @@ class BusinessSupportSchemesControllerTest < ActionController::TestCase
       business_support_identifier: "urban-development-grant")
     business_mentoring = FactoryGirl.create(:business_support_scheme, title: "Business Mentoring",
       business_support_identifier: "business-mentoring")
+    sectorless_scheme = FactoryGirl.create(:business_support_scheme, title: "I have no sectors",
+      business_support_identifier: "i-have-no-sectors")
     start_up = FactoryGirl.create(:business_support_stage, name: "Start-up", slug: "start-up")
     grow_sustain = FactoryGirl.create(:business_support_stage, name: "Grow and sustain", slug: "grow-and-sustain")
     
@@ -41,22 +43,30 @@ class BusinessSupportSchemesControllerTest < ActionController::TestCase
     business_mentoring.business_support_types << loan
     business_mentoring.business_support_stages << grow_sustain
     
+    sectorless_scheme.business_support_business_types << charity
+    sectorless_scheme.business_support_locations << scotland
+    sectorless_scheme.business_support_stages << start_up
+
     eu_culture_programme.save!
     urban_dev_grant.save!
     business_mentoring.save!
+    sectorless_scheme.save!
+
   end
 
   test "GET to index no params" do
     get :index, format: :json
     json = JSON.parse(response.body)
-    assert_equal 3, json['total'].to_i
+    assert_equal 4, json['total'].to_i
   end
 
   test "GET to index with gibberish params" do
     get :index, format: :json, sectors: 'foo,bar'
     result = JSON.parse(response.body)
     assert_equal 'ok', result['_response_info']['status']
-    assert_equal 0, result['total']  
+    assert_equal 2, result['total']
+    assert_equal "Business Mentoring", result['results'].first['title']
+    assert_equal "I have no sectors", result['results'].last['title']
   end
  
   test "GET to index with multiple sectors" do
@@ -70,7 +80,8 @@ class BusinessSupportSchemesControllerTest < ActionController::TestCase
       business_types: 'charity', stages: 'grow-and-sustain', locations: 'scotland',
       types: 'award'
     results = JSON.parse(response.body)['results']
-    assert_equal "EU Culture Programme", results.first['title']
+    assert_equal "Business Mentoring", results.first['title']
+    assert_equal "EU Culture Programme", results.second['title']
   end
   
   test "GET to index with no sectors param" do
@@ -79,7 +90,7 @@ class BusinessSupportSchemesControllerTest < ActionController::TestCase
       types: 'award'
     results = JSON.parse(response.body)['results']
     assert_equal "Business Mentoring", results.first['title']
-    assert_equal "EU Culture Programme", results.last['title']
+    assert_equal "EU Culture Programme", results.second['title']
   end
   
   test "GET to index with multiple params for location and business type" do
@@ -87,9 +98,10 @@ class BusinessSupportSchemesControllerTest < ActionController::TestCase
       locations: 'scotland,wales', types: 'award,loan'
     json = JSON.parse(response.body)
     results = json['results']
-    assert_equal 3, json['total'].to_i
+    assert_equal 4, json['total'].to_i
     assert_equal "Business Mentoring", results.first['title']
     assert_equal "EU Culture Programme", results.second['title']
-    assert_equal "Urban Development Grant", results.last['title']
+    assert_equal "I have no sectors", results.third['title']
+    assert_equal "Urban Development Grant", results.fourth['title']
   end
 end
