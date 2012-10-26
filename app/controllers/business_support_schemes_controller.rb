ba978491db29bd804ebe375a9944ab2b517e8432
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2012-10-26 16:25:49 +0000

    Refactor controller methods into scope

--- app/controllers/business_support_schemes_controller.rb
+++ app/controllers/business_support_schemes_controller.rb
@@ -3,12 +3,11 @@ class BusinessSupportSchemesController < ApplicationController
   RELATIONAL_KEYS = [:business_types, :sectors, :stages, :locations, :types]
 
   def index
-    criteria = schemes_criteria
-    if criteria.empty?
+    relations = params.keep_if{ |k,v| RELATIONAL_KEYS.include?(k.to_sym) }
+    if relations.empty?
       schemes = BusinessSupportScheme.asc(:title)
     else
-      relations = params.keep_if{ |k,v| RELATIONAL_KEYS.include?(k.to_sym) }
-      schemes = BusinessSupportScheme.where({ "$and" => schemes_criteria }).asc(:title)
+      schemes = BusinessSupportScheme.for_relations(relations)
     end 
     @count = schemes.size
     @schemes_json = schemes.to_json(only: [:business_support_identifier, :title]) 
@@ -17,26 +16,4 @@ class BusinessSupportSchemesController < ApplicationController
     end
   end
 
-  private
-
-  def schemes_criteria
-    criteria = []
-    RELATIONAL_KEYS.each do |sym|
-      unless params[sym].nil?
-        collection = "business_support_#{sym.to_s.singularize}_ids".to_sym
-        collection_ids = ids_for_slugs(sym)
-        selector = { collection => [] }
-        unless collection_ids.empty?
-          selector = { "$or" => [{ collection => { "$in" => collection_ids } }, selector] }
-        end
-        criteria << selector
-      end
-    end
-    criteria
-  end
-
-  def ids_for_slugs(sym)
-    klass = Kernel.const_get("BusinessSupport#{sym.to_s.singularize.camelize}")
-    klass.any_in(slug: params[sym].split(',')).map(&:id)  
-  end
 end
