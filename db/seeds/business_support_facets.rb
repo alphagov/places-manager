af4dec7dab14b8ccb95705c9316bd9709cf4cbae
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2012-11-07 17:35:56 +0000

    Seed BSF facets

---
+++ db/seeds/business_support_facets.rb
@@ -0,0 +1,45 @@
+# BusinessSupportBusinessType
+BusinessSupportBusinessType.find_or_create_by(:name => "Private Company", :slug => "private-company")
+BusinessSupportBusinessType.find_or_create_by(:name => "Public limited company", :slug => "public-limited-company")
+BusinessSupportBusinessType.find_or_create_by(:name => "Partnership", :slug => "partnership")
+BusinessSupportBusinessType.find_or_create_by(:name => "Social enterprise", :slug => "social-enterprise")
+BusinessSupportBusinessType.find_or_create_by(:name => "Charity", :slug => "charity")
+BusinessSupportBusinessType.find_or_create_by(:name => "Sole trader", :slug => "sole-trader")
+
+# BusinessSupportLocation
+BusinessSupportLocation.find_or_create_by(:name => "Northern Ireland", :slug => "northern-ireland")
+BusinessSupportLocation.find_or_create_by(:name => "England", :slug => "england")
+BusinessSupportLocation.find_or_create_by(:name => "Wales", :slug => "wales")
+BusinessSupportLocation.find_or_create_by(:name => "Scotland", :slug => "scotland")
+
+# BusinessSupportSector
+BusinessSupportSector.find_or_create_by(:name => "Wholesale and Retail", :slug => "wholesale-and-retail")
+BusinessSupportSector.find_or_create_by(:name => "Manufacturing", :slug => "manufacturing")
+BusinessSupportSector.find_or_create_by(:name => "Hospitality and Catering", :slug => "hospitality")
+BusinessSupportSector.find_or_create_by(:name => "Travel and Leisure", :slug => "travel-and-leisure")
+BusinessSupportSector.find_or_create_by(:name => "Agriculture", :slug => "agriculture")
+BusinessSupportSector.find_or_create_by(:name => "Construction", :slug => "construction")
+BusinessSupportSector.find_or_create_by(:name => "Information, Communication and Media", :slug => "information-communication-and-media")
+BusinessSupportSector.find_or_create_by(:name => "Science and Technology", :slug => "science-and-technology")
+BusinessSupportSector.find_or_create_by(:name => "Transport and Distribution", :slug => "transport-and-distribution")
+BusinessSupportSector.find_or_create_by(:name => "Utilities", :slug => "utilities")
+BusinessSupportSector.find_or_create_by(:name => "Business and Finance", :slug => "business-and-finance")
+BusinessSupportSector.find_or_create_by(:name => "Education", :slug => "education")
+BusinessSupportSector.find_or_create_by(:name => "Health", :slug => "health")
+BusinessSupportSector.find_or_create_by(:name => "Service Industries", :slug => "service-industries")
+BusinessSupportSector.find_or_create_by(:name => "Mining", :slug => "mining")
+BusinessSupportSector.find_or_create_by(:name => "Real Estate", :slug => "real-estate")
+
+# BusinessSupportStage
+BusinessSupportStage.find_or_create_by(:name => "Pre-startup", :slug => "pre-startup")
+BusinessSupportStage.find_or_create_by(:name => "Start-up", :slug => "start-up")
+BusinessSupportStage.find_or_create_by(:name => "Grow and sustain", :slug => "grow-and-sustain")
+BusinessSupportStage.find_or_create_by(:name => "Exiting a business", :slug => "exiting-a-business")
+
+# BusinessSupportType
+BusinessSupportType.find_or_create_by(:name => "Grant", :slug => "grant")
+BusinessSupportType.find_or_create_by(:name => "Finance", :slug => "finance")
+BusinessSupportType.find_or_create_by(:name => "Loan", :slug => "loan")
+BusinessSupportType.find_or_create_by(:name => "Expertise and Advice", :slug => "expertise-and-advice")
+BusinessSupportType.find_or_create_by(:name => "Recognition Award", :slug => "recognition-award")
+BusinessSupportType.find_or_create_by(:name => "Equity", :slug => "equity")
