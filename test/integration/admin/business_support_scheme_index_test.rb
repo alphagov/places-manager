d70ef46555390ad826a520034262e8a480d56ddb
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2012-11-09 17:49:27 +0000

    Add test missed out in earlier commit

---
+++ test/integration/admin/business_support_scheme_index_test.rb
@@ -0,0 +1,24 @@
+require_relative '../../integration_test_helper'
+require_relative '../../business_support_test_helper'
+
+class BusinessSupportSchemeIndexTest < ActionDispatch::IntegrationTest
+
+  setup do
+    ["Super finance triple bonus", "Young business starter award", "Brilliant start-up award", "Wunderbiz"
+      ].each_with_index do |title, index|
+        FactoryGirl.create(:business_support_scheme, 
+                          title: title, 
+                          business_support_identifier: index + 1,
+                          priority: 1)
+    end
+  end
+
+  test "the index page" do
+    visit "/admin/business_support_schemes"
+    
+    assert page.has_content?("Brilliant start-up award")
+    assert page.has_content?("Super finance triple bonus")
+    assert page.has_content?("Young business starter award")
+    assert page.has_content?("Wunderbiz")
+  end
+end
