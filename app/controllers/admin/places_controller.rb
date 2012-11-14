76f404ef3b00be100ae234b2c5203a89056138eb
Jordan Hatch <jordan@jordanh.net>
2012-11-14 13:04:07 +0000

    Prevent controller actions where places cannot be edited.
    Added functional tests for updating places.

--- app/controllers/admin/places_controller.rb
+++ app/controllers/admin/places_controller.rb
@@ -4,6 +4,18 @@ class Admin::PlacesController < InheritedResources::Base
   belongs_to :service
   belongs_to :data_set
 
+  def edit
+    unless resource.can_edit?
+      flash[:alert] = 'You cannot edit this place as ' + (data_set.active? ? 'this data set is currently active.' : "there is a more recent data set available.")
+      redirect_to admin_service_data_set_path(@service, @data_set)
+    end
+  end
+
+  def update
+    head(:unprocessable_entity) and return unless resource.can_edit?
+    update!
+  end
+
   protected
     def service
       @service ||= Service.find(params['service_id'])
