6b1d36b15cfb7671418fef4113f463ee8fd51e2d
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2013-02-05 16:34:07 +0000

    Write output to local files
    Without this, it gets sent in an email to machine.email@...

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 set :output, {:error => 'log/cron.error.log', :standard => 'log/cron.log'}
-job_type :run_script, 'cd :path && /usr/local/bin/govuk_setenv imminence bundle exec script/:task'
+job_type :run_script, 'cd :path && /usr/local/bin/govuk_setenv imminence bundle exec script/:task :output'
 
 every 15.minutes do
   run_script "geocoder"
