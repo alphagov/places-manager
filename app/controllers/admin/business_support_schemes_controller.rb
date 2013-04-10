d28547bcc8f0d9102c02b0351a827d63acd6a656
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-04-10 16:04:05 +0000

    Export local business support data as CSV

--- app/controllers/admin/business_support_schemes_controller.rb
+++ app/controllers/admin/business_support_schemes_controller.rb
@@ -7,6 +7,12 @@ class Admin::BusinessSupportSchemesController < InheritedResources::Base
 
   def index
     @schemes = BusinessSupportScheme.asc(:title)
+    respond_to do |format|
+      format.csv do
+        send_data generate_schemes_csv(@schemes), :filename => 'business_support_schemes.csv'
+      end
+      format.all
+    end
   end
 
   def new
@@ -39,6 +45,32 @@ class Admin::BusinessSupportSchemesController < InheritedResources::Base
     end
   end
 
+protected
+
+  def generate_schemes_csv(schemes)
+    CSV.generate do |csv|
+      csv << [
+        "id","title",
+        #"web_url","organiser","short description","body",
+        #"eligibility","evaluation","additional information","contact details",
+        #"max employees","min value","max value","continuation link",
+        "business types","locations","purposes",
+        "sectors","stages","support types",
+      ]
+      schemes.each do |scheme|
+        csv << [
+          scheme.business_support_identifier, scheme.title,
+          # TODO: populate these fields from ContentAPI
+          #nil, nil, nil, nil,
+          #nil, nil, nil, nil,
+          #nil, nil, nil, nil,
+          scheme.business_types.join(','), scheme.locations.join(','), scheme.purposes.join(','),
+          scheme.sectors.join(','), scheme.stages.join(','), scheme.support_types.join(','),
+        ]
+      end
+    end
+  end
+
   def find_all_facets
     @business_types = BusinessSupportBusinessType.asc(:name)
     @locations = BusinessSupportLocation.asc(:name)
@@ -47,5 +79,4 @@ class Admin::BusinessSupportSchemesController < InheritedResources::Base
     @stages = BusinessSupportStage.asc(:name)
     @types = BusinessSupportType.asc(:name)
   end
-
 end
