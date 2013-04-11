95adfc7a704c76e62469f3da9f07cd438f5d33c8
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-04-11 14:45:49 +0000

    Remove redundant comments

--- app/presenters/business_support_csv_presenter.rb
+++ app/presenters/business_support_csv_presenter.rb
@@ -9,19 +9,12 @@ class BusinessSupportCSVPresenter
     CSV.generate do |csv|
       csv << [
         "id","title",
-        #"web_url","organiser","short description","body",
-        #"eligibility","evaluation","additional information","contact details",
-        #"max employees","min value","max value","continuation link",
         "business types","locations","purposes",
         "sectors","stages","support types",
       ]
       @schemes.each do |scheme|
         csv << [
           scheme.business_support_identifier, scheme.title,
-          # TODO: populate these fields from ContentAPI
-          #nil, nil, nil, nil,
-          #nil, nil, nil, nil,
-          #nil, nil, nil, nil,
           scheme.business_types.join(','), scheme.locations.join(','), scheme.purposes.join(','),
           scheme.sectors.join(','), scheme.stages.join(','), scheme.support_types.join(','),
         ]
