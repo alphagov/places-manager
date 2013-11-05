94be3db2e39bb979c95f31619ccac88c74ba4d2f
David Heath <david.heath@digital.cabinet-office.gov.uk>
2013-11-05 16:32:47 +0000

    Configure imminence to use 4 worker processes by default

---
+++ config/unicorn.rb
@@ -0,0 +1,7 @@
+# Load the system-wide standard Unicorn file
+def load_file_if_exists(config, file)
+  config.instance_eval(File.read(file)) if File.exist?(file)
+end
+load_file_if_exists(self, "/etc/govuk/unicorn.rb")
+working_directory File.dirname(File.dirname(__FILE__))
+worker_processes 4
