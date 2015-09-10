803cc1be04f95598292ff43af38c072ef28660c0
Michael Patrick <michael.patrick@digital.cabinet-office.gov.uk>
2015-09-10 16:19:13 +0100

    Remove duplicate specs
    These two spec contexts claim to be testing different scenarios, but aside from
    a single assertion* are merely repeating the same thing twice.
    
    * It's not clear to me what value the single differentiating assertion in
    question is adding, but it holds true for both contexts either way, so I'm
    leaving it in there.

--- test/unit/presenters/areas_presenter_test.rb
+++ test/unit/presenters/areas_presenter_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 require 'areas_presenter'
 
 class AreasPresenterTest < ActiveSupport::TestCase
-  context "presenting an areas by type mapit response" do
+  context "presenting a mapit response" do
     setup do
       bridge = OpenStruct.new(:payload => {
         :code => 200,
@@ -47,67 +47,6 @@ class AreasPresenterTest < ActiveSupport::TestCase
         assert_equal "E09000033", @result["codes"]["gss"]
 
         refute @result.has_key?("parent_area")
-      end
-    end
-
-    context "second result" do
-      setup do
-        @result = @presenter.present["results"][1]
-      end
-
-      should "expose the correct data" do
-        assert_equal "london", @result["slug"]
-        assert_equal "London", @result["name"]
-        assert_equal "England", @result["country_name"]
-        assert_equal "EUR", @result["type"]
-        assert_equal "E15000007", @result["codes"]["gss"]
-      end
-    end
-  end
-
-  context "presenting a postcode lookup mapit response" do
-    setup do
-      response = OpenStruct.new(:payload => {
-        :code => 200,
-        :areas => [
-          {
-            "id" => 123,
-            "name" => "Westminster City Council",
-            "country_name" => "England",
-            "type" => "LBO",
-            "codes" => {
-              "gss" => "E09000033",
-            },
-          },
-          {
-            "id" => 234,
-            "name" => "London",
-            "country_name" => "England",
-            "type" => "EUR",
-            "codes" => {
-              "gss" => "E15000007",
-            },
-          },
-        ],
-      })
-      @presenter = AreasPresenter.new(response)
-    end
-
-    should "format the correct response status" do
-      assert_equal "ok", @presenter.present["_response_info"]["status"]
-    end
-
-    context "first result" do
-      setup do
-        @result = @presenter.present["results"].first
-      end
-
-      should "expose the correct data" do
-        assert_equal "westminster-city-council", @result["slug"]
-        assert_equal "Westminster City Council", @result["name"]
-        assert_equal "England", @result["country_name"]
-        assert_equal "LBO", @result["type"]
-        assert_equal "E09000033", @result["codes"]["gss"]
       end
     end
 
