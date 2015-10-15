524c216f97fdd5b3f7a94229dcb7a901e024c050
Murray Steele <muz@h-lame.com>
2015-10-15 12:25:23 +0100

    Setup ci-reporter for jenkins correctly
    Now that we have to include testunit and minitest the app seems to think it's
    using testunit not minitest so the ci:setup:minitest task doesn't end up
    generating the report files.  This means that our build won't pass as a lack of
    reports is a considered a failure by ci.
    
    Once we're on rails 4.x we can review again which framework we are using for tests; minitest vs. test-unit and only invoke one of the setup tasks.
    
    Tried to add reporting for cucumber, but no version worked satisfactorially. It
    either made cucumber not load any features, and thus report an error or trapped
    the cucumber output so the ci console reported none and you had to click
    through to the junit reports to see the problems.  This doesn't seem like a great idea as it changes dev workflow and not neccessarily for the better.  So
    we left it out.  Perhaps we can revisit later.

--- jenkins.sh
+++ jenkins.sh
@@ -10,4 +10,6 @@ fi
 
 bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
 bundle exec rake db:drop
-bundle exec rake ci:setup:minitest default
+# We only need one of the minitest or testunit setup tasks, but we'll fix it
+# when we jump to rails 4.x and decide which one it is
+bundle exec rake ci:setup:minitest ci:setup:testunit default
