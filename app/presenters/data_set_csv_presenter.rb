3633ae2759dd55f584a72fa695d2bc599afca954
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2015-12-31 14:23:30 +0000

    Factor out private methods on DataSetCsvPresenter

--- app/presenters/data_set_csv_presenter.rb
+++ app/presenters/data_set_csv_presenter.rb
@@ -17,28 +17,48 @@ class DataSetCsvPresenter
 
   def to_array_for_csv
     [].tap do |csv|
-      # We need to handle the location fields separately from the others.
-      # location is a Point object (so doesn't serialize well to CSV by default)
-      # and doesn't need to be exported because it's either overridden from the
-      # override fields or set from MapIt's data using the postcode.
-      # Also exclude the id from the CSV since it isn't needed by the import.
-
-      # The fields called 'lng' and 'lat' are used as the override values by
-      # Place.parameters_from_hash so we need to name them accordingly in the
-      # CSV to avoid them being lost if a CSV is exported and then imported.
-      non_location_headers = Place.attribute_names - ['_id', 'location', 'override_lng', 'override_lat']
-      location_headers = ['lng', 'lat']
-      csv << (non_location_headers + location_headers)
+      csv << all_headers
       places.each do |place|
-        row = non_location_headers.collect { |h| place.send(h.to_sym) }
-        if place.override_lat_lng?
-          row << place.override_lng
-          row << place.override_lat
-        else
-          2.times { |n| row << nil }
-        end
-        csv << row
+        csv << build_row(place)
       end
+    end
+  end
+
+private
+
+  # We need to handle the location fields separately from the others.
+  # location is a Point object (so doesn't serialize well to CSV by default)
+  # and doesn't need to be exported because it's either overridden from the
+  # override fields or set from MapIt's data using the postcode.
+  # Also exclude the id from the CSV since it isn't needed by the import.
+  def non_location_headers
+    @non_location_headers ||= Place.attribute_names - ['_id', 'location', 'override_lng', 'override_lat']
+  end
+
+  # The fields called 'lng' and 'lat' are used as the override values by
+  # Place.parameters_from_hash so we need to name them accordingly in the
+  # CSV to avoid them being lost if a CSV is exported and then imported.
+  def location_headers
+    ['lng', 'lat']
+  end
+
+  def all_headers
+    non_location_headers + location_headers
+  end
+
+  def build_row(place)
+    non_location_fields(place) + location_fields(place)
+  end
+
+  def non_location_fields(place)
+    non_location_headers.map { |header| place.send(header.to_sym) }
+  end
+
+  def location_fields(place)
+    if place.override_lat_lng?
+      [place.override_lng, place.override_lat]
+    else
+      [nil, nil]
     end
   end
 end
