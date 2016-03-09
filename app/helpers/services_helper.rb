171059515de8ba5810e58bddad1e5e98d099e9fe
Murray Steele <muz@h-lame.com>
2016-03-09 12:27:04 +0000

    Add UI for choosing the local authority hierarchy type
    A simple dropdown; the hint text might need work.

--- app/helpers/services_helper.rb
+++ app/helpers/services_helper.rb
@@ -2,4 +2,8 @@ module ServicesHelper
   def service_location_match_type_options
     Service::LOCATION_MATCH_TYPES.map { |location_match_type| [location_match_type.humanize, location_match_type] }
   end
+
+  def service_local_authority_hierarchy_match_type_options
+    Service::LOCAL_AUTHORITY_HIERARCHY_MATCH_TYPES.map { |match_type| [match_type.humanize, match_type] }
+  end
 end
