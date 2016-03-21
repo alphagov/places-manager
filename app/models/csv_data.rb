a5e6e7ec2d63e68dadec25973d24e2af6c05c246
Murray Steele <muz@h-lame.com>
2016-03-21 17:11:32 +0000

    Create CsvData model
    This is to hold CSV data uploaded for new data sets.  These will not be
    stored embedded in the data set (which is in turn embedded in the
    service).  This means we avoid issues with breaching per-document limits
    on size if any csv processing job is killed without completing.
    
    We have followed the pattern on Place for storing the connection between
    the csv data and the data set it "belongs to".  The reason for doing it
    this way rather than using a traditional has_one/belongs_to reference
    relationship is that mongoid does not support these when one end of the
    association is an embedded document.

---
+++ app/models/csv_data.rb
@@ -0,0 +1,51 @@
+class CsvData
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :service_slug, type: String
+  field :data_set_version, type: Integer
+  field :data, type: String
+
+  # Mongoid has a 16M limit on document size.  Set this to
+  # 15M to leave some headroom for storing the rest of the document.
+  validates :data, length: { maximum: 15.megabytes, message: "CSV file is too big (max is 15MB)" }
+  validates_presence_of :service_slug
+  validates_presence_of :data_set_version
+  validates_presence_of :data
+
+  def service
+    @_service ||= Service.where(slug: service_slug).first
+  end
+
+  def data_set
+    @_data_set ||= service.data_sets.where(version: data_set_version).first if service
+  end
+
+  def data_file=(file)
+    self.data =
+      if file.nil?
+        nil
+      else
+        read_as_utf8(file)
+      end
+  end
+
+private
+
+  def read_as_utf8(file)
+    string = file.read.force_encoding('utf-8')
+    unless string.valid_encoding?
+      # Try windows-1252 (which is a superset of iso-8859-1)
+      string.force_encoding('windows-1252')
+      # Any stream of bytes should be a vaild Windows-1252 string, so we use the presence of any
+      # ASCII control chars (except for \r and \n) as a good heuristic to determine if this is
+      # likely to be the correct charset
+      if string.valid_encoding? && ! string.match(/[\x00-\x09\x0b\x0c\x0e-\x1f]/)
+        return string.encode('utf-8')
+      end
+
+      raise InvalidCharacterEncodingError, "Unknown character encoding"
+    end
+    string
+  end
+end
---
+++ test/unit/csv_data_test.rb
@@ -0,0 +1,104 @@
+require 'test_helper'
+
+class CsvDataTest < ActiveSupport::TestCase
+  context 'associations' do
+    setup do
+      @service = Service.create! slug: 'chickens', name: 'Chickens!'
+      @data_set = @service.data_sets.create! version: 2
+      @csv_data = CsvData.create!(
+        service_slug: 'chickens',
+        data_set_version: 2,
+        data: "1,2,3"
+      )
+    end
+
+    should "can look up the service it belongs to" do
+      assert_equal @service, @csv_data.service
+    end
+
+    should "can look up the data set it belongs to" do
+      s = Service.create! slug: 'ducks', name: 'Ducks!'
+      s.data_sets.create! version: 2
+
+      assert_equal @data_set, @csv_data.data_set
+    end
+  end
+
+  context "validations" do
+    should 'be invalid without a service slug' do
+      csv_data = CsvData.create(service_slug: nil, data_set_version: 2, data: '1,2,3')
+      refute csv_data.valid?
+      assert_equal 1, csv_data.errors[:service_slug].size
+    end
+
+    should 'be invalid without a data set version' do
+      csv_data = CsvData.create(service_slug: 'foo', data_set_version: nil, data: '1,2,3')
+      refute csv_data.valid?
+      assert_equal 1, csv_data.errors[:data_set_version].size
+    end
+
+    should 'be invalid without data' do
+      csv_data = CsvData.create(service_slug: 'foo', data_set_version: 2, data: nil)
+      refute csv_data.valid?
+      assert_equal 1, csv_data.errors[:data].size
+    end
+
+    context "validating file size" do
+      setup do
+        @service = Service.create! slug: 'chickens', name: 'Chickens!'
+        @data_set = @service.data_sets.create! version: 2
+      end
+
+      should "be valid with a file up to 15M" do
+        csv_data = CsvData.create(service_slug: 'chickens', data_set_version: 2, data: "x" * (15.megabytes - 1))
+        assert csv_data.valid?
+      end
+
+      should "be invalid with a file over 15M" do
+        csv_data = CsvData.create(service_slug: 'chickens', data_set_version: 2, data: "x" * (15.megabytes + 1))
+        refute csv_data.valid?
+        assert_equal 1, csv_data.errors[:data].size
+      end
+    end
+
+    context "handling various file encodings" do
+      setup do
+        @csv_data = CsvData.new(service_slug: 'foo', data_set_version: 2)
+      end
+
+      should "handle ASCII files" do
+        @csv_data.data_file = File.open(fixture_file_path('encodings/ascii.csv'), encoding: 'ascii-8bit')
+        @csv_data.save!
+        expected = File.read(fixture_file_path('encodings/ascii.csv'))
+        assert_equal expected, @csv_data.data
+      end
+
+      should "handle UTF-8 files" do
+        @csv_data.data_file = File.open(fixture_file_path('encodings/utf-8.csv'), encoding: 'ascii-8bit')
+        @csv_data.save!
+        expected = File.read(fixture_file_path('encodings/utf-8.csv'))
+        assert_equal expected, @csv_data.data
+      end
+
+      should "handle ISO-8859-1 files" do
+        @csv_data.data_file = File.open(fixture_file_path('encodings/iso-8859-1.csv'), encoding: 'ascii-8bit')
+        @csv_data.save!
+        expected = File.read(fixture_file_path('encodings/iso-8859-1.csv')).force_encoding('iso-8859-1').encode('utf-8')
+        assert_equal expected, @csv_data.data
+      end
+
+      should "handle Windows 1252 files" do
+        @csv_data.data_file = File.open(fixture_file_path('encodings/windows-1252.csv'), encoding: 'ascii-8bit')
+        @csv_data.save!
+        expected = File.read(fixture_file_path('encodings/windows-1252.csv')).force_encoding('windows-1252').encode('utf-8')
+        assert_equal expected, @csv_data.data
+      end
+
+      should "raise an error with an unknown file encoding" do
+        assert_raise InvalidCharacterEncodingError do
+          @csv_data.data_file = File.open(fixture_file_path('encodings/utf-16le.csv'), encoding: 'ascii-8bit')
+        end
+      end
+    end
+  end
+end
