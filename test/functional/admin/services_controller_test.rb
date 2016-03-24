5428f264ed928aa690460b9434399dc8f867c758
David Basalla <davidbasalla@gmail.com>
2016-03-24 17:39:30 +0000

    Allow local_authority_hierarchy_match_type param

--- test/functional/admin/services_controller_test.rb
+++ test/functional/admin/services_controller_test.rb
@@ -1,5 +1,27 @@
 require 'test_helper'
 
 class Admin::ServicesControllerTest < ActionController::TestCase
-  # TODO: Write some controller tests here
+  test "should create service" do
+    as_logged_in_user do
+      service_params = {
+        "name" => "Register Offices",
+        "slug" => "register-offices",
+        "location_match_type" => "local_authority",
+        "local_authority_hierarchy_match_type" => "county"
+      }
+
+      post :create, service: service_params
+      assert_response :redirect
+
+      assert_equal 1, Service.count
+
+      service = Service.last
+      assert_equal "Register Offices", service.name
+      assert_equal "register-offices", service.slug
+      assert_equal "local_authority", service.location_match_type
+      assert_equal "county", service.local_authority_hierarchy_match_type
+    end
+  end
+
+  # TODO: Write more controller tests here
 end
