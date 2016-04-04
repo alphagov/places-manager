a035cf10d7a5b0889917857e0d35e413341d35d2
David Basalla <davidbasalla@gmail.com>
2016-04-04 14:21:04 +0000

    Move Service feature tests into separate file
    Both Service and Dataset features were in one file. We separate the features and step definitions into their respective files.

--- features/step_definitions/data_set_steps.rb
+++ features/step_definitions/data_set_steps.rb
@@ -1,21 +1,9 @@
-Given /^I have previously created the "(.*?)" service$/ do |name|
-  @service = create_service(name)
-end
-
 Given /^I have uploaded a (second|third) data set$/ do |ordinal|
   upload_extra_data_set(@service)
 end
 
 Given /^background processing has completed$/ do
   run_all_delayed_jobs
-end
-
-When /^I go to the new service page$/ do
-  visit new_admin_service_path
-end
-
-When /^I go to the page for the "(.*?)" service$/ do |name|
-  visit path_for_service(name)
 end
 
 When /^I upload a new data set$/ do
@@ -39,50 +27,12 @@ When /^I upload a new data set with a PNG claiming to be a CSV$/ do
   end
 end
 
-When /^I visit the details tab$/ do
-  click_link 'Service details'
-end
-
-When /^I visit the history tab$/ do
-  click_link 'Version history'
-end
-
 When /^I click "Activate"$/ do
   click_button 'Activate'
 end
 
 When /^I click "Duplicate"$/ do
   click_button 'Duplicate'
-end
-
-When /^I fill in the form to create the "(.*?)" service with a bad CSV$/ do |name|
-  params = {
-    name: name,
-    slug: name.parameterize,
-    csv_path: Rails.root.join('features/support/data/bad.csv')
-  }
-
-  fill_in_form_with(params)
-end
-
-When /^I fill in the form to create the "(.*?)" service with a PNG claiming to be a CSV$/ do |name|
-  params = {
-    name: name,
-    slug: name.parameterize,
-    csv_path: Rails.root.join('features/support/data/rails.csv')
-  }
-
-  fill_in_form_with(params)
-end
-
-When /^I fill in the form to create the "(.*?)" service with a PNG$/ do |name|
-  params = {
-    name: name,
-    slug: name.parameterize,
-    csv_path: Rails.root.join('features/support/data/rails.png')
-  }
-
-  fill_in_form_with(params)
 end
 
 When /^I go to the page for the latest data set for the "(.*?)" service$/ do |name|
@@ -137,23 +87,6 @@ Then /^show me the page$/ do
   save_and_open_page
 end
 
-When /^I fill out the form with the following attributes to create a service:$/ do |table|
-  params = table.rows_hash.symbolize_keys!
-  raise ArgumentError, "name cannot be nil" if params[:name].nil?
-
-  params = {
-    slug: params[:name].parameterize,
-    csv_path: csv_path_for_data(params[:name])
-  }.merge(params)
-
-  fill_in_form_with(params)
-end
-
-Then /^I should be on the page for the "(.*?)" service$/ do |name|
-  current_path = URI.parse(current_url).path
-  assert_equal path_for_service(name), current_path
-end
-
 Then /^I should be on the page for the latest data set for the "(.*?)" service$/ do |name|
   current_path = URI.parse(current_url).path
   assert_equal path_for_latest_data_set_for_service(name), current_path
@@ -189,10 +122,6 @@ end
 
 Then /^the "(.*?)" service should have two data sets$/ do |name|
   assert_equal 2, Service.where(name: name).first.data_sets.count
-end
-
-Then /^there should not be a "(.*?)" service$/ do |name|
-  assert_equal 0, Service.where(name: name).count
 end
 
 Then /^there should be a place named "(.*?)"$/ do |name|
---
+++ features/step_definitions/service_steps.rb
@@ -0,0 +1,70 @@
+Given /^I have previously created the "(.*?)" service$/ do |name|
+  @service = create_service(name)
+end
+
+When /^I go to the new service page$/ do
+  visit new_admin_service_path
+end
+
+When /^I go to the page for the "(.*?)" service$/ do |name|
+  visit path_for_service(name)
+end
+
+When /^I visit the details tab$/ do
+  click_link 'Service details'
+end
+
+When /^I visit the history tab$/ do
+  click_link 'Version history'
+end
+
+When /^I fill in the form to create the "(.*?)" service with a bad CSV$/ do |name|
+  params = {
+    name: name,
+    slug: name.parameterize,
+    csv_path: Rails.root.join('features/support/data/bad.csv')
+  }
+
+  fill_in_form_with(params)
+end
+
+When /^I fill in the form to create the "(.*?)" service with a PNG claiming to be a CSV$/ do |name|
+  params = {
+    name: name,
+    slug: name.parameterize,
+    csv_path: Rails.root.join('features/support/data/rails.csv')
+  }
+
+  fill_in_form_with(params)
+end
+
+When /^I fill in the form to create the "(.*?)" service with a PNG$/ do |name|
+  params = {
+    name: name,
+    slug: name.parameterize,
+    csv_path: Rails.root.join('features/support/data/rails.png')
+  }
+
+  fill_in_form_with(params)
+end
+
+When /^I fill out the form with the following attributes to create a service:$/ do |table|
+  params = table.rows_hash.symbolize_keys!
+  raise ArgumentError, "name cannot be nil" if params[:name].nil?
+
+  params = {
+    slug: params[:name].parameterize,
+    csv_path: csv_path_for_data(params[:name])
+  }.merge(params)
+
+  fill_in_form_with(params)
+end
+
+Then /^I should be on the page for the "(.*?)" service$/ do |name|
+  current_path = URI.parse(current_url).path
+  assert_equal path_for_service(name), current_path
+end
+
+Then /^there should not be a "(.*?)" service$/ do |name|
+  assert_equal 0, Service.where(name: name).count
+end
