d659bf7ca92f74e63fee7023981e4bb21afd7294
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2016-10-27 16:40:08 +0000

    Switch the replaced files at deploy with ENV vars
    This is part of ongoing work to make our apps operate in more of a
    12-factor fashion.
    
    As part of this we'll be updating the deployment strategy to not replace
    files at deploy time and differentiating any keys shared between
    environments.

--- config/initializers/gds-sso.rb
+++ config/initializers/gds-sso.rb
@@ -1,6 +1,6 @@
 GDS::SSO.config do |config|
-  config.user_model   = 'User'
-  config.oauth_id     = "abcdefgh12345678imm"
-  config.oauth_secret = "secret"
+  config.user_model   = "User"
+  config.oauth_id     = ENV["OAUTH_ID"]
+  config.oauth_secret = ENV["OAUTH_SECRET"]
   config.oauth_root_url = Plek.current.find("signon")
 end
