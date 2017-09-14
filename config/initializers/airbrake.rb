6f7a9f7fc652fae25d7623d0236ec9c23b005040
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2017-09-14 11:15:38 +0100

    Switch to Sentry from Airbrake
    Mostly following instructions from https://docs.publishing.service.gov.uk/manual/upgrade-to-sentry.html
    but also switching from sidekiq to govuk_sidekiq.

--- config/initializers/airbrake.rb
+++
@@ -1,8 +0,0 @@
-if ENV["ERRBIT_API_KEY"].present?
-  Airbrake.configure do |config|
-    config.api_key = ENV["ERRBIT_API_KEY"]
-    config.host = "errbit.#{ENV['GOVUK_APP_DOMAIN']}"
-    config.secure = true
-    config.environment_name = ENV["ERRBIT_ENVIRONMENT_NAME"]
-  end
-end
