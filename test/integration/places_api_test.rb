4c11976804ee26ee49c6d98fe125572608f64949
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2019-10-01 14:20:18 +0100

    Fix Naming/VariableNumber offences

--- test/integration/places_api_test.rb
+++ test/integration/places_api_test.rb
@@ -7,12 +7,12 @@ class PlacesAPITest < ActionDispatch::IntegrationTest
   context "Requesting the full dataset" do
     setup do
       @service = FactoryBot.create(:service)
-      @data_set_1 = @service.active_data_set
-      @data_set_2 = @service.data_sets.create
+      @data_set1 = @service.active_data_set
+      @data_set2 = @service.data_sets.create
       @place_1a = FactoryBot.create(
         :place,
         service_slug: @service.slug,
-        data_set_version: @data_set_1.version,
+        data_set_version: @data_set1.version,
         latitude: 51.613314,
         longitude: -0.158278,
         name: "Town Hall",
@@ -20,7 +20,7 @@ class PlacesAPITest < ActionDispatch::IntegrationTest
       @place_1b = FactoryBot.create(
         :place,
         service_slug: @service.slug,
-        data_set_version: @data_set_1.version,
+        data_set_version: @data_set1.version,
         latitude: 51.500728,
         longitude: -0.124626,
         name: "Palace of Westminster",
@@ -28,7 +28,7 @@ class PlacesAPITest < ActionDispatch::IntegrationTest
       @place_2a = FactoryBot.create(
         :place,
         service_slug: @service.slug,
-        data_set_version: @data_set_2.version,
+        data_set_version: @data_set2.version,
         latitude: 51.613314,
         longitude: -0.158278,
         name: "Town Hall 2",
@@ -36,12 +36,12 @@ class PlacesAPITest < ActionDispatch::IntegrationTest
       @place_2b = FactoryBot.create(
         :place,
         service_slug: @service.slug,
-        data_set_version: @data_set_2.version,
+        data_set_version: @data_set2.version,
         latitude: 51.500728,
         longitude: -0.124626,
         name: "Palace of Westminster 2",
       )
-      @data_set_2.activate
+      @data_set2.activate
     end
 
     should "return all places for the current dataset as JSON" do
@@ -77,17 +77,17 @@ class PlacesAPITest < ActionDispatch::IntegrationTest
         GDS::SSO.test_user = FactoryBot.create(:user)
         visit "/admin" # necessary to setup the login session
 
-        visit "/places/#{@service.slug}.json?version=#{@data_set_1.to_param}"
+        visit "/places/#{@service.slug}.json?version=#{@data_set1.to_param}"
 
         data = JSON.parse(page.source)
         assert_equal ["Palace of Westminster", "Town Hall"], (data.map { |p| p["name"] })
       end
 
       should "ignore requested version and return active version when not logged in" do
-        visit "/places/#{@service.slug}.json?version=#{@data_set_1.to_param}"
+        visit "/places/#{@service.slug}.json?version=#{@data_set1.to_param}"
 
         data = JSON.parse(page.source)
-        # Titles from @data_set_2
+        # Titles from @data_set2
         assert_equal ["Palace of Westminster 2", "Town Hall 2"], (data.map { |p| p["name"] })
       end
     end
