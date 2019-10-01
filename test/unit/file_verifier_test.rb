d168557b10954f159af469c2260e8d030f53d0bb
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2019-10-01 14:27:53 +0100

    Fix Rails/FilePath offences

--- test/unit/file_verifier_test.rb
+++ test/unit/file_verifier_test.rb
@@ -3,27 +3,27 @@ require "imminence/file_verifier"
 
 class FileVerifierTest < ActiveSupport::TestCase
   test "it can be handed a file object" do
-    f = File.open(Rails.root.join("features/support/data/register-offices.csv"))
+    f = File.open(Rails.root.join("features", "support", "data", "register-offices.csv"))
     assert_equal "text/plain", Imminence::FileVerifier.new(f).mime_type
   end
 
   test "it can be handed a path" do
-    f = Rails.root.join("features/support/data/register-offices.csv")
+    f = Rails.root.join("features", "support", "data", "register-offices.csv")
     assert_equal "text/plain", Imminence::FileVerifier.new(f).mime_type
   end
 
   test "it correctly identifies a PNG masquerading as a CSV" do
-    f = Rails.root.join("features/support/data/rails.csv")
+    f = Rails.root.join("features", "support", "data", "rails.csv")
     assert_equal "image/png", Imminence::FileVerifier.new(f).mime_type
   end
 
   test "it can provide just the main type of a file" do
-    f = Rails.root.join("features/support/data/rails.csv")
+    f = Rails.root.join("features", "support", "data", "rails.csv")
     assert_equal "image", Imminence::FileVerifier.new(f).type
   end
 
   test "it can provide just the sub-type of a file" do
-    f = Rails.root.join("features/support/data/rails.csv")
+    f = Rails.root.join("features", "support", "data", "rails.csv")
     assert_equal "png", Imminence::FileVerifier.new(f).sub_type
   end
 end
