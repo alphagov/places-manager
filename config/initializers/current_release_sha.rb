3d70a0f45061db7c6dd3fd22d73b8a688e757d70
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2019-10-02 13:16:28 +0100

    Replace `cat` in the current_release_sha initializer

--- config/initializers/current_release_sha.rb
+++ config/initializers/current_release_sha.rb
@@ -1,5 +1,5 @@
 if File.exist?(Rails.root.join("REVISION"))
-  revision = `cat #{Rails.root.join("REVISION")}`.chomp
+  revision = File.read(Rails.root.join("REVISION")).chomp
   CURRENT_RELEASE_SHA = revision[0..10] # Just get the short SHA
 else
   CURRENT_RELEASE_SHA = "development".freeze
