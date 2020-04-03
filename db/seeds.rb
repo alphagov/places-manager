d511a9b6e16392ab38fbfc8856b066db1778e79b
Michael Walker <mike@barrucadu.co.uk>
2020-04-03 08:45:09 +0100

    Fix Layout/SpaceInsideStringInterpolation

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
 Dir[File.join(File.dirname(__FILE__), "seeds", "*.rb")].each do |f|
-  puts "Seeding from #{ File.basename f }..."
+  puts "Seeding from #{File.basename f}..."
   load f
   puts "Done."
 end
