62e85ba3dda7eb4059ecb6fa6fa21ced35140d31
Ben <benbaumann95@hotmail.co.uk>
2020-07-14 16:22:42 +0100

    Add development hosts to config.hosts
    Rails 6.0 adds a host authentication middleware that limits access by
    default to 0.0.0.0/0, ::/0 and localhost. This commit adds the dev.gov.uk
    hosts for the application to that list. There's no need to add the hosts
    in production since the middleware is primarily to prevent DNS rebinding
    attacks against a Rails application running locally.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.hosts += [
+    "imminence.dev.gov.uk",
+  ]
 end
