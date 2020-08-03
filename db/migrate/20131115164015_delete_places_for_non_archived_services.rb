2a1f763b34331de8735a3832f5f92072baf9b624
Issy Long <me@issyl0.co.uk>
2020-08-03 14:29:37 +0100

    Remove usage of `mongoid_rails_migrations` gem & all migrations
    - To [pay down some tech debt](https://trello.com/c/CUj0jrkN/184-were-using-an-old-forked-gem-for-mongodb-migrations-that-isnt-necessary),
      we're removing this gem and all data migrations.
    - While this repo does not use our forked branch of the gem (like other
      apps did), we should still tidy its use up for consistency with all of
      our MongoDB applications.
    - MongoDB is schemaless, and one would not expect to see migrations in a
      MongoDB project.
    - All of these migrations were data migrations from 2013 to 2020, and
      most likely (tho untested) won't apply from scratch any more (this is
      why we rely on replicated data).
    - Our preferred approach for data changes these days is to write
      short-lived Rake tasks.

--- db/migrate/20131115164015_delete_places_for_non_archived_services.rb
+++
@@ -1,8 +0,0 @@
-class DeletePlacesForNonArchivedServices < Mongoid::Migration
-  def self.up
-    PlaceArchive.delete_all(conditions: {service_slug: { "$nin" => ["number-plate-supplier", "motorcycle-approved-training-bodies"]}})
-  end
-
-  def self.down
-  end
-end
