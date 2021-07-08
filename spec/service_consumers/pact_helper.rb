7f97d2859d12b717178ffd05d54c380d5cc57225
Callum Knights <callum.knights@digital.cabinet-office.gov.uk>
2021-07-08 14:28:13 +0100

    Add a provider_state for the new test
    This includes stubbing out the response from Mapit, as we are just testing
    the api call from the consumer (gds-api-adapters) to imminence, we shoudn't
    be adding an additional api-call into the mix.

--- spec/service_consumers/pact_helper.rb
+++ spec/service_consumers/pact_helper.rb
@@ -14,6 +14,8 @@ end
 
 WebMock.allow_net_connect!
 
+MAPIT_ENDPOINT = Plek.current.find("mapit")
+
 def url_encode(str)
   ERB::Util.url_encode(str)
 end
@@ -42,6 +44,45 @@ Pact.provider_states_for "GDS API Adapters" do
     set_up do
       service = create(:service, slug: "number-plate-supplier")
       create(:place, service_slug: service.slug, latitude: 50.742754933617285, longitude: -1.9552618901330387)
+    end
+  end
+
+  provider_state "a place exists with a postcode and areas" do
+    set_up do
+      service = create(:service, slug: "local-authority")
+      create(:place, service_slug: service.slug, postcode: "WC2B 6SE")
+
+      areas = [
+        { "name" => "Westminster City Council", "type" => "LBO" },
+        { "name" => "London", "type" => "EUR" },
+      ]
+
+      response = {
+        "wgs84_lat" => 51.516,
+        "wgs84_lon" => -0.121,
+        "postcode" => "WC2B 6SE",
+      }
+
+      area_response = Hash[areas.map.with_index do |area, i|
+        [i,
+         {
+           "codes" => {
+             "ons" => area["ons"],
+             "gss" => area["gss"],
+             "govuk_slug" => area["govuk_slug"],
+           },
+           "name" => area["name"],
+           "type" => area["type"],
+           "country_name" => area["country_name"],
+         }]
+      end]
+
+      postcode = "WC2B 6SE"
+
+      stub_request(:get, "#{MAPIT_ENDPOINT}/postcode/#{postcode.tr(' ', '+')}.json")
+        .to_return(body: response.merge("areas" => area_response).to_json, status: 200)
+      stub_request(:get, "#{MAPIT_ENDPOINT}/postcode/partial/#{postcode.split(' ').first}.json")
+        .to_return(body: response.to_json, status: 200)
     end
   end
 end
