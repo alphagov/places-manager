2c64e0e3a6ab3717c17263d96a7585604f54cc89
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2022-08-15 11:21:36 +0100

    Update places_near to use PostGIS with distance order
    Order has to be reorder because of default name scope
    Also ignore default name scope when checking latest data set.

--- app/models/place.rb
+++ app/models/place.rb
@@ -57,8 +57,8 @@ class Place < ApplicationRecord
   # Convert mongoid's geo_near_distance attribute to a Distance object
   # so that we can easily convert it to other units.
   def dis
-    if attributes["geo_near_distance"]
-      @dis ||= Distance.new(attributes["geo_near_distance"], :degrees)
+    if attributes["distance"]
+      @dis ||= Distance.new(attributes["distance"], :meters)
     end
   end
 
