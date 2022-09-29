8a84eb17c397ef398d1d827d11b91e6eea59305e
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2022-09-29 14:04:13 +0100

    Consolidates factories
    Combines factories into one file, as there aren't many and it was
    confusing to find a Service factory from inside the places factory file.

--- test/factories/factories.rb
+++ test/factories/factories.rb
@@ -1,4 +1,11 @@
 FactoryBot.define do
+  factory :user do
+    sequence(:uid) { |n| "uid-#{n}" }
+    sequence(:name) { |n| "Joe Bloggs #{n}" }
+    sequence(:email) { |n| "joe#{n}@bloggs.com" }
+    permissions { %w[signin] }
+  end
+
   factory :service do
     name { "Important Government Service" }
     sequence(:slug) { |n| "important-government-service-#{n}" }
