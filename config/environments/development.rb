9b5b3c308724da8e28b50e58ca94db5dbc1cf83b
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2023-11-29 07:05:14 +0000

    Reinstate config.hosts.clear

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Allow requests for all domains e.g. <app>.dev.gov.uk
+  config.hosts.clear
 end
