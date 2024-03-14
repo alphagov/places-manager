f4cea5f51a46a931478dfd47dfc5613fdd848550
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-03-14 13:44:21 +0000

    Update layout, include components
    - Add styles for action sideblock, a la Local Links Manager
    - Remove unused helper

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,14 +1,4 @@
-//= require jquery
-//= require bootstrap
-//= require 'modules/form-value-dependent'
-
-
-/**
- * Activate the tab specified by the location hash
- */
-$(document).ready(function () {
-  var $tabToActivate = $('.nav-tabs a[href="' + window.location.hash + '"]');
-  if ($tabToActivate.length) {
-    $tabToActivate.tab('show');
-  }
-});
+//= require govuk_publishing_components/dependencies
+//= require govuk_publishing_components/lib
+//= require govuk_publishing_components/components/layout-header
+//= require govuk_publishing_components/components/table
