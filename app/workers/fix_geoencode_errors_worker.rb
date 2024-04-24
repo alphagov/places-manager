6ad631876b7c289317e4a34262c50f656e2281bd
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-04-24 16:41:03 +0100

    Remove not very useful fix-geocode worker

--- app/workers/fix_geoencode_errors_worker.rb
+++
@@ -1,7 +0,0 @@
-class FixGeoencodeErrorsWorker
-  include Sidekiq::Worker
-
-  def perform(service_id, data_set_version)
-    Service.find(service_id).data_sets.find_by(version: data_set_version).places.with_geocoding_errors.each(&:geocode!)
-  end
-end
