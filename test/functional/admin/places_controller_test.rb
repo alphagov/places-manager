badcb308919e73d1de9e1c98136baa1813d0f5df
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-04-24 13:44:28 +0100

    Add basic tests for Admin PlacesController

--- test/functional/admin/places_controller_test.rb
+++ test/functional/admin/places_controller_test.rb
@@ -2,6 +2,24 @@ require "test_helper"
 
 class Admin::PlacesControllerTest < ActionController::TestCase
   setup do
-    @service = FactoryBot.create(:service)
+    @data_set = FactoryBot.create(:data_set)
+  end
+
+  context "GET (:service/:data_set/:place)" do
+    should "display place details" do
+      as_gds_editor do
+        get :show, params: { service_id: @data_set.service.id, data_set_id: @data_set.id, id: @data_set.places.first.id }
+
+        assert_response(:success)
+      end
+    end
+
+    should "reject if user is not in the appropriate department" do
+      as_other_department_user do
+        get :show, params: { service_id: @data_set.service.id, data_set_id: @data_set.id, id: @data_set.places.first.id }
+
+        assert_response(:forbidden)
+      end
+    end
   end
 end
