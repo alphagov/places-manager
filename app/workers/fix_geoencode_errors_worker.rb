79336b50014ff72f14f82e898bbc1aad0690893d
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-04-29 09:17:17 +0100

    Fix fix-geocode-worker

--- app/workers/fix_geoencode_errors_worker.rb
+++ app/workers/fix_geoencode_errors_worker.rb
@@ -2,6 +2,6 @@ class FixGeoencodeErrorsWorker
   include Sidekiq::Worker
 
   def perform(service_id, data_set_version)
-    Service.find(service_id).data_sets.where(version: data_set_version).places.with_geocoding_errors.each(&:geoencode!)
+    Service.find(service_id).data_sets.find_by(version: data_set_version).places.with_geocoding_errors.each(&:geocode!)
   end
 end
