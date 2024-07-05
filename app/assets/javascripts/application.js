c117b2c70a7306340603cd10939f8ddcff75d176
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2024-07-05 09:45:20 +0100

    JavaScript changes for govuk-frontend v5
    - updates the application JavaScript to reflect required changes coming with the govuk-frontend v5 upgrade
    - moves some of the JavaScript into a separate script file that will not be opened by legacy browsers, because it contains modern JS that they will not recognise and error on

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,3 @@
 //= require govuk_publishing_components/dependencies
 //= require govuk_publishing_components/lib
-//= require govuk_publishing_components/components/layout-header
 //= require govuk_publishing_components/components/table
