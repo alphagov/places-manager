c97afff21d9f3b5aaf358b11f4ff510c5699015a
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-09-04 17:14:02 +0100

    Handle items that are in search but not content-store gracefully

--- spec/services/govuk_site_lookup_service_spec.rb
+++ spec/services/govuk_site_lookup_service_spec.rb
@@ -1,11 +1,13 @@
 require "rails_helper"
 
 RSpec.describe(GovukSiteLookupService, type: :model) do
+  let(:search_results) { { results: [{ title: "Test Service", link: "/test-service-page" }] } }
+
   before do
-    search_results = { results: [{ title: "Test Service", link: "/test-service-page" }] }
     stub_request(:get, "http://search-api.dev.gov.uk/search.json?count=200&fields=title,link&filter_format=place").to_return(status: 200, body: search_results.to_json, headers: {})
     content_item = { details: { place_type: "test-service" } }
     stub_request(:get, "http://content-store.dev.gov.uk/content/test-service-page").to_return(status: 200, body: content_item.to_json, headers: {})
+    stub_request(:get, "http://content-store.dev.gov.uk/content/foooo").to_return(status: 404, body: "", headers: {})
   end
 
   describe "#govuk_page?" do
@@ -26,6 +28,14 @@ RSpec.describe(GovukSiteLookupService, type: :model) do
 
   describe "#page_title" do
     it "returns title of frontend page" do
+      expect(GovukSiteLookupService.new.page_title("test-service")).to(eq("Test Service"))
+    end
+  end
+
+  context "with items that are in search but missing from content-store" do
+    let(:search_results) { { results: [{ title: "foo", link: "/foooo" }, { title: "Test Service", link: "/test-service-page" }] } }
+
+    it "returns title of frontend page, ignoring the broken item" do
       expect(GovukSiteLookupService.new.page_title("test-service")).to(eq("Test Service"))
     end
   end
