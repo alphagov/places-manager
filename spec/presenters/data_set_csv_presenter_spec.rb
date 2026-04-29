3748f08cbb6cf88719f2069796a2e26d366dd21f
deborahchua <19667619+deborahchua@users.noreply.github.com>
2026-04-29 15:51:19 +0100

    Refactor DataSetCsvPresenter and tests
    The database migration causes some tests to fail
    as the presenter gets the column fields from
    the Places model (which has now been reordered
    to be alphabetical).
    
    This commit simplifies the presenter so that the
    column names are now hardcoded in a constant, and
    we also only really care about the `lat` and `lng`
    fields.
    
    We also tidy up and fix the tests which are now
    passing.

--- spec/presenters/data_set_csv_presenter_spec.rb
+++ spec/presenters/data_set_csv_presenter_spec.rb
@@ -1,34 +1,30 @@
 require "rails_helper"
 
 RSpec.describe(DataSetCsvPresenter, type: :model) do
-  before do
-    @service = FactoryBot.create(:service)
-    @data_set = @service.data_sets.create!
-    @presenter = DataSetCsvPresenter.new(@data_set)
-    @result = @presenter.to_array_for_csv
-  end
+  subject(:data_set_csv_presenter) { described_class.new(data_set) }
+  let(:service) { FactoryBot.create(:service) }
+  let(:data_set) { service.data_sets.create! }
+  let(:result) { data_set_csv_presenter.to_csv }
 
   def expected_header_row
-    %w[service_slug data_set_version name source_address address1 address2 town postcode access_notes general_notes url email phone fax text_phone geocode_error gss lng lat]
+    "service_slug,data_set_version,name,source_address,address1,address2,town,postcode,access_notes,general_notes,url,email,phone,fax,text_phone,geocode_error,gss,lng,lat"
   end
 
   context "presenting an empty data set" do
     it "contain only a header row" do
-      expect(@result).to(eq([expected_header_row]))
+      expect(result.chomp).to(eq(expected_header_row))
     end
   end
 
   context "presenting a data set with a place" do
-    before do
-      @place = FactoryBot.create(:place, service_slug: @service.slug, data_set_version: @data_set.version, email: "camden@example.com", gss: "00AG", override_lng: 0.0, override_lat: 1.0)
-      @result = @presenter.to_array_for_csv
-    end
+    let(:place) { FactoryBot.create(:place, service_slug: service.slug, data_set_version: data_set.version, email: "camden@example.com", gss: "00AG", override_lng: 0.0, override_lat: 1.0) }
 
     it "contain a header row and a results row" do
-      expected_place_row = [@service.slug, @data_set.version, @place.name, @place.source_address, @place.address1, @place.address2, @place.town, @place.postcode, @place.access_notes, @place.general_notes, @place.url, @place.email, @place.phone, @place.fax, @place.text_phone, @place.geocode_error, @place.gss, @place.override_lng, @place.override_lat]
-      expect(@result.size).to(eq(2))
-      expect(@result.first).to(eq(expected_header_row))
-      expect(@result.last).to(eq(expected_place_row))
+      expected_place_row = "#{service.slug},2,CosaNostra Pizza #3569,\"#{place.address1}, Los Angeles, WC2B 6NH\",#{place.address1},,Los Angeles,WC2B 6NH,,,,camden@example.com,01234 567890,,,,00AG,0.0,1.0"
+
+      expect(result.split("\n").size).to(eq(2))
+      expect(result.split("\n").first).to(eq(expected_header_row))
+      expect(result.split("\n").last).to(eq(expected_place_row))
     end
   end
 end
