3dac101aee847df82c9db0f0bb3cd42f60d9ec01
deborahchua <deborah.chua@dsit.gov.uk>
2026-08-27 13:37:43 +0100

    Remove use of StatsD and StatsCollector
    We have now configured the app to report to
    Prometheus. We also already report metrics
    on controller/actions to Prometheus so it
    is not necessary to have custom configuration
    anymore.

--- config/initializers/statsd.rb
+++
@@ -1,8 +0,0 @@
-require "places_manager/stats_collector"
-
-ActiveSupport::Notifications.subscribe(/process_action.action_controller/) do |*args|
-  event = ActiveSupport::Notifications::Event.new(*args)
-  PlacesManager::StatsCollector.timing(
-    event.duration, event.payload[:controller].underscore, event.payload[:action]
-  )
-end
